8c5a33c232d8ed8fcff933559225f8eb2221cdd4
ernieayala <teafortwo.ea@gmail.com>
2020-07-21 19:10:28 -0700

    Themeing

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Ernie's Modern UI
 A visual update for Foundry VTT UI.
 
-There is also a "compact mode" and the ability to change the primary color.
+There is also a "compact mode" and the ability to theme different areas of the application.
 
 Covers most UI items but still a huge work in progress. There is a ton of redundant CSS but this is because I don't want to step on the globals just yet.
 
 Will cover more parts of the app as time goes on.
+
+Not all the modules will have my styling. Make an issue on Github if you want me to look at one.
 
 **Only tested on Chrome so far.**
 
@@ -24,53 +26,5 @@ https://raw.githubusercontent.com/ernieayala/ernies-modern-layout/master/module.
 <img src="https://github.com/ernieayala/ernies-modern-layout/raw/master/images/eml-compact.jpg"
      style="max-width: 100%;" />
 
-### Tested With These Modules Active
-- About Time
-- Batch Permission By Folder
-- Better Roll Tables
-- Better Rolls 5e
-- Calendar/Weather
-- Cautious Gamemaster's Pack
-- Combat Template Cleanup
-- Combat Utility Belt
-- Compendium Hider
-- Cursor Hider
-- Custom Hotbar
-- Dancing Lights
-- Dice Tray
-- Dungeondraft Importer
-- Dynamic Effects
-- Dynamic Illumination
-- Follow Me
-- Foriens Quest Log
-- Foriens Scene Navigator
-- Foundry Community Macros
-- Foundry Community Tables
-- FxMaster
-- Haste
-- Health Monitor
-- Journal Sync
-- Let Me Roll That For You
-- Loot Sheet NPC 5e
-- Macro Marker
-- Magic Items
-- Merge Walls
-- Permission Viewer
-- Response Time
-- RollTable Button
-- Show Notes
-- The Furnace
-- Tidy UI - Game Settings
-- Tidy 5e Sheet
-- Token Action HUD
-- Token Hotbar
-- Token Mold
-- Trigger Happy
-- Turn Marker
-- Tweak Playlist
-- Wall Height
-
-Not all the modules will have my styling. Make an issue on Github if you want me to look at one.
-
 ## Contact
-Make issues on Github or message me on Discord: Teafortwo#4453
+Make issues on Github or message me on Discord: Teafortwo#4453 and/or Ernie
--- js/eml.js
+++ js/eml.js
@@ -1,3 +1,6 @@
+const myHtml = document.getElementsByTagName('html');
+const myBody = document.getElementsByTagName('body');
+
 // Hex to RGB function
 function convertHex(color){
 	let hex = color.replace('#','');
@@ -10,33 +13,213 @@ function convertHex(color){
 
 // Used to create an ultra specific selector
 Hooks.once('init', () => {
-	let myHtml = document.getElementsByTagName('html');
-	let myBody = document.getElementsByTagName('body');
-
 	// lets makes make this specificity stupid
 	myBody[0].classList.add('emu');
 	myBody[0].classList.add('e-body');
 	myBody[0].setAttribute('id', 'emu');
+});
+
+Hooks.once('ready', () => {
+	let defaultColorPicker = true;
 
 	// Theme
-	game.settings.register('ernies-modern-layout', 'primaryColor', {
-		name: 'Primary Theme Color',
-		hint: 'Use a full length hex value. Will make it better later. Default Value: #e57509 - Blue: #386acf - Green: #0fbd7d - Purple: #773edc - Red: #dc143c - Yellow: #edb950',
-		scope: 'user',
-		config: true,
-		default: '#E57509',
-		type: String,
-		onChange: data => {
-			if(data.length === 7) {
-				let rgbValue = convertHex(data);
-				rgbValue ? document.documentElement.style.setProperty('--color-primary', rgbValue) : null;
+	if(defaultColorPicker) {
+		game.settings.register('ernies-modern-layout', 'colorPrimary', {
+			name: 'Primary Theme Color',
+			hint: 'Default Value: #e57509 - Blue: #386acf - Green: #0fbd7d - Purple: #773edc - Red: #dc143c - Yellow: #edb950',
+			scope: 'user',
+			config: true,
+			default: '#E57509',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-primary', convertHex(data));
 			}
-		}
-	});
-	const primaryColor = game.settings.get('ernies-modern-layout', 'primaryColor');
-	if(primaryColor && primaryColor.length === 7) {
-		let rgbValue = convertHex(primaryColor);
-		rgbValue ? document.documentElement.style.setProperty('--color-primary', rgbValue): null;
+		});
+		const colorPrimary = game.settings.get('ernies-modern-layout', 'colorPrimary');
+		colorPrimary ? document.documentElement.style.setProperty('--color-primary', convertHex(colorPrimary)) : null;
+
+		// Background Default
+		game.settings.register('ernies-modern-layout', 'colorBackground', {
+			name: 'Background Default',
+			hint: 'Deault: #293e40',
+			scope: 'user',
+			config: true,
+			default: '#293e40',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-background', convertHex(data));
+			}
+		});
+		const colorBackground = game.settings.get('ernies-modern-layout', 'colorBackground');
+		colorBackground ? document.documentElement.style.setProperty('--color-background', convertHex(colorBackground)) : null;
+
+		// Background Lightest
+		game.settings.register('ernies-modern-layout', 'colorBackgroundLightest', {
+			name: 'Background Lightest',
+			hint: 'Deault: #e6e9eb',
+			scope: 'user',
+			config: true,
+			default: '#e6e9eb',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-background-lightest', convertHex(data));
+			}
+		});
+		const colorBackgroundLightest = game.settings.get('ernies-modern-layout', 'colorBackgroundLightest');
+		colorBackgroundLightest ? document.documentElement.style.setProperty('--color-background-lightest', convertHex(colorBackgroundLightest)) : null;
+
+		// Background Light
+		game.settings.register('ernies-modern-layout', 'colorBackgroundLight', {
+			name: 'Background Light',
+			hint: 'Deault: #7d8a8c',
+			scope: 'user',
+			config: true,
+			default: '#7d8a8c',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-background-light', convertHex(data));
+			}
+		});
+		const colorBackgroundLight = game.settings.get('ernies-modern-layout', 'colorBackgroundLight');
+		colorBackgroundLight ? document.documentElement.style.setProperty('--color-background-light', convertHex(colorBackgroundLight)) : null;
+
+		// Background Darkest
+		game.settings.register('ernies-modern-layout', 'colorBackgroundDarkest', {
+			name: 'Background Darkest',
+			hint: 'Deault: #090e10',
+			scope: 'user',
+			config: true,
+			default: '#090e10',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-background-darkest', convertHex(data));
+			}
+		});
+		const colorBackgroundDarkest = game.settings.get('ernies-modern-layout', 'colorBackgroundDarkest');
+		colorBackgroundDarkest ? document.documentElement.style.setProperty('--color-background-darkest', convertHex(colorBackgroundDarkest)) : null;
+
+		// Background Button
+		game.settings.register('ernies-modern-layout', 'colorBackgroundButton', {
+			name: 'Background Button',
+			hint: 'Deault: #7d7d7d',
+			scope: 'user',
+			config: true,
+			default: '#7d7d7d',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-background-button', convertHex(data));
+			}
+		});
+		const colorBackgroundButton = game.settings.get('ernies-modern-layout', 'colorBackgroundButton');
+		colorBackgroundButton ? document.documentElement.style.setProperty('--color-background-button', convertHex(colorBackgroundButton)) : null;
+
+		// Border
+		game.settings.register('ernies-modern-layout', 'colorBorder', {
+			name: 'Border',
+			hint: 'Deault: #213234',
+			scope: 'user',
+			config: true,
+			default: '#213234',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-border', convertHex(data));
+			}
+		});
+		const colorBorder = game.settings.get('ernies-modern-layout', 'colorBorder');
+		colorBorder ? document.documentElement.style.setProperty('--color-border', convertHex(colorBorder)) : null;
+
+		// Folder Header
+		game.settings.register('ernies-modern-layout', 'colorFolderHeader', {
+			name: 'Folder Header',
+			hint: 'Deault: #a7b0b2',
+			scope: 'user',
+			config: true,
+			default: '#a7b0b2',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-folder-header', convertHex(data));
+			}
+		});
+		const colorFolderHeader = game.settings.get('ernies-modern-layout', 'colorFolderHeader');
+		colorFolderHeader ? document.documentElement.style.setProperty('--color-folder-header', convertHex(colorFolderHeader)) : null;
+
+		// Folder Directory
+		game.settings.register('ernies-modern-layout', 'colorFolderDirectory', {
+			name: 'Folder Directory',
+			hint: 'Deault: #536466',
+			scope: 'user',
+			config: true,
+			default: '#536466',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-folder-directory', convertHex(data));
+			}
+		});
+		const colorFolderDirectory = game.settings.get('ernies-modern-layout', 'colorFolderDirectory');
+		colorFolderDirectory ? document.documentElement.style.setProperty('--color-folder-directory', convertHex(colorFolderDirectory)) : null;
+
+		// Folder Subdirectory
+		game.settings.register('ernies-modern-layout', 'colorFolderSubdirectory', {
+			name: 'Folder Subdirectory',
+			hint: 'Deault: #d1d6d8',
+			scope: 'user',
+			config: true,
+			default: '#d1d6d8',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-folder-subdirectory', convertHex(data));
+			}
+		});
+		const colorFolderSubdirectory = game.settings.get('ernies-modern-layout', 'colorFolderSubdirectory');
+		colorFolderSubdirectory ? document.documentElement.style.setProperty('--color-folder-subdirectory', convertHex(colorFolderSubdirectory)) : null;
+
+		// Text
+		game.settings.register('ernies-modern-layout', 'colorText', {
+			name: 'Text',
+			hint: 'Deault: #090E10',
+			scope: 'user',
+			config: true,
+			default: '#090E10',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-text', convertHex(data));
+			}
+		});
+		const colorText = game.settings.get('ernies-modern-layout', 'colorText');
+		colorText ? document.documentElement.style.setProperty('--color-text', convertHex(colorText)) : null;
+
+		// Text Lightest
+		game.settings.register('ernies-modern-layout', 'colorTextLightest', {
+			name: 'Text Lightest',
+			hint: 'Deault: #ffffff',
+			scope: 'user',
+			config: true,
+			default: '#ffffff',
+			type: String,
+			onChange: data => {
+				document.documentElement.style.setProperty('--color-text-lightest', convertHex(data));
+			}
+		});
+		const colorTextLightest = game.settings.get('ernies-modern-layout', 'colorTextLightest');
+		colorTextLightest ? document.documentElement.style.setProperty('--color-text-lightest', convertHex(colorTextLightest)) : null;
+	} else {
+		// new window.Ardittristan.ColorSetting('ernies-modern-layout', 'primaryColor', {
+		// 	name: 'Primary Color',
+		// 	hint: 'Default Value: #e57509',
+		// 	label: 'Primary Color',
+		// 	restricted: false,
+		// 	defaultColor: '#E57509',
+		// 	scope: 'client',
+		// 	onChange: (value) => {
+		// 		let rgbValue = convertHex(value);
+		// 		rgbValue ? document.documentElement.style.setProperty('--color-primary', rgbValue) : null;
+		// 	}
+		// });
+		// const primaryColor = game.settings.get('ernies-modern-layout', 'primaryColor');
+		// if(primaryColor) {
+		// 	let rgbValue = convertHex(primaryColor);
+		// 	rgbValue ? document.documentElement.style.setProperty('--color-primary', rgbValue) : null;
+		// }
 	}
 
 	// Layouts
