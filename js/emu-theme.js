b7f317fac5e85cf93a4431a88b7c01c653647144
Ernesto Ayala <teafortwo.ea@gmail.com>
2020-11-18 18:06:25 -0800

    new theme and bug fixes

---
+++ js/emu-theme.js
@@ -0,0 +1,59 @@
+export const FOUNDRY = {
+	colorPrimary: '#e57509',
+	colorBackground: '#293e40',
+	colorBackgroundLightest: '#e6e9eb',
+	colorBackgroundLight: '#7d8a8c',
+	colorBackgroundDarkest: '#090e10',
+	colorBackgroundButton: '#7d7d7d',
+	colorBackgroundChatMessage: '#e6e9eb',
+	colorBackgroundChatMessageWhisper: '#ecf1fc',
+	colorBackgroundChatMessageBlind: '#ffecf0',
+	colorBorder: '#213234',
+	colorBorderLighter: '#a7b0b2',
+	colorFolderHeader: '#a7b0b2',
+	colorFolderDirectory: '#536466',
+	colorFolderSubdirectory: '#d1d6d8',
+	colorText: '#090e10',
+	colorTextLightest: '#ffffff',
+	colorTextDarker: '#293e40'
+}
+
+export const DARK = {
+	colorPrimary: '#111a1c',
+	colorBackground: '#2e2e2e',
+	colorBackgroundLightest: '#666666',
+	colorBackgroundLight: '#4a4a4a',
+	colorBackgroundDarkest: '#151515',
+	colorBackgroundButton: '#111a1c',
+	colorBackgroundChatMessage: '#666666',
+	colorBackgroundChatMessageWhisper: '#192628',
+	colorBackgroundChatMessageBlind: '#293E40',
+	colorBorder: '#4a4a4a',
+	colorBorderLighter: '#7d7d7d',
+	colorFolderHeader: '#4a4a4a',
+	colorFolderDirectory: '#151515',
+	colorFolderSubdirectory: '#7d7d7d',
+	colorText: '#f1f1f1',
+	colorTextLightest: '#fafafa',
+	colorTextDarker: '#cbcbcb'
+}
+
+export const WESTERN = {
+	colorPrimary: '#ef972d',
+	colorBackground: '#4e321c',
+	colorBackgroundLightest: '#eddabe',
+	colorBackgroundLight: '#f0cb94',
+	colorBackgroundDarkest: '#492819',
+	colorBackgroundButton: '#83613c',
+	colorBackgroundChatMessage: '#eadac3',
+	colorBackgroundChatMessageWhisper: '#ecd6b4',
+	colorBackgroundChatMessageBlind: '#f0cb94',
+	colorBorder: '#4d321c',
+	colorBorderLighter: '#baae86',
+	colorFolderHeader: '#c09769',
+	colorFolderDirectory: '#83613c',
+	colorFolderSubdirectory: '#8e704a',
+	colorText: '#111111',
+	colorTextLightest: '#ffffff',
+	colorTextDarker: '#000000'
+}
